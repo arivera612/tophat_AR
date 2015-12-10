bc2a01951bf34606df76839b3660d8c8906999d7
arivera612 <arive019@ucr.edu>
2015-12-09 22:42:09 -0800

    Update Tophat_1.sh

--- Tophat_1.sh
+++ Tophat_1.sh
@@ -1,10 +1,16 @@
 input=$1
+
+#load modules
 module load samtools
 module load bowtie2
 module load tophat
+
+#sets directory and output as the basename of input
 dir=`dirname $input`
 output=`basename $input .fastq`
 echo $dir
+
+# requires annotation file and reference fasta file
 tophat -p 4 -g 20 --segment-length 25 -i 50 -I 500000 -G /bigdata/messaoudilab/arivera/GEN220/groupproject/2.Alignment/Tophat/features.gff -o /bigdata/messaoudilab/arivera/GEN220/groupproject/2.Alignment/Tophat/$output /bigdata/messaoudilab/arivera/GEN220/groupproject/2.Alignment/Tophat/genome.fasta $input
 
 
